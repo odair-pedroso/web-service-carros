f6380c3cb9c3326292ce3d32ba2c5b4813fdc2bb
odair-pedroso <odairpedroso90@gmail.com>
2021-03-15 08:29:52 -0300

    metodo DAO lista carros pelo nome

--- src/br/com/livro/domain/CarroDAO.java
+++ src/br/com/livro/domain/CarroDAO.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CarroDAO extends BaseDAO {
 
@@ -37,13 +39,32 @@ public class CarroDAO extends BaseDAO {
 	}
 
 	// método que retorna uma lista de carros pelo nome ;
-	public List<Carro> findByName (String nome) throws SQLException{
-		
+	public List<Carro> findByName(String nome) throws SQLException {
+
 		List<Carro> carros = new ArrayList<>();
 		Connection conn = null;
 		PreparedStatement stmt = null;
-		
-		conn = getConnection();
-		
-		
+
+		try {
+			conn = getConnection();
+			stmt = conn.prepareStatement("select * from carro where lower(nome) like?");
+			stmt.setString(1, "%" + name.toLowerCase() + "%");
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				Carro c = createCarro(rs);
+				carros.add(c);
+			}
+			rs.close();
+		} finally {
+			if (stmt != null) {
+				stmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+
+		}
+		return carros;
 	}
+
+}
